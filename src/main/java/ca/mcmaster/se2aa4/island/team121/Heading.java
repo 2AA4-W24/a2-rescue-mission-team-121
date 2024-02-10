5e0f4759bc67cbeb2f778556bcbb63b58623060a
zayedu <umerzayed1@gmail.com>
2024-02-10 13:57:59 -0500

    feat: Added Heading Enum

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team121/Heading.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.se2aa4.island.team121;
+
+public enum Heading {
+    NORTH("North"),
+    SOUTH("South"),
+    EAST("East"),
+    WEST("West");
+    private final String vector;
+
+    private Heading(String vector) {
+        this.vector = vector;
+    }
+}
