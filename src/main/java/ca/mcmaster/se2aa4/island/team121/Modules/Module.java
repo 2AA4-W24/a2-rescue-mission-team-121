c0905b2baa8073754f14a5b5a9733e3691c394d5
zayedu <umerzayed1@gmail.com>
2024-02-21 17:57:53 -0500

    feat: Completed module testing

--- src/main/java/ca/mcmaster/se2aa4/island/team121/Modules/Module.java
+++
@@ -1,9 +0,0 @@
-package ca.mcmaster.se2aa4.island.team121.Modules;
-
-import ca.mcmaster.se2aa4.island.team121.Decision;
-import org.json.JSONObject;
-
-public interface Module {
-    JSONObject getJSON();
-    Decision getDecision();
-}
--- src/main/java/ca/mcmaster/se2aa4/island/team121/TileType.java
+++
@@ -1,3 +0,0 @@
-package ca.mcmaster.se2aa4.island.team121;
-
-public enum TileType {Ground, Water, Emergency, Creek}
