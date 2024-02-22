5b08e6a5c3e479e682793a6b530f149b0907419d
williampol <103951515+williampol@users.noreply.github.com>
2024-02-22 17:57:47 -0500

    feat: Add updateTurn and logs

--- src/main/java/ca/mcmaster/se2aa4/island/team121/Records/MapUpdater.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team121/Records/MapUpdater.java
@@ -1,7 +1,9 @@
 package ca.mcmaster.se2aa4.island.team121.Records;
 
+import ca.mcmaster.se2aa4.island.team121.Heading;
+
 public interface MapUpdater {
     void updateFly();
-    void updateTurn();
+    void updateTurn(Heading new_heading);
     void updateScan();
 }
--- src/main/java/ca/mcmaster/se2aa4/island/team121/Records/RelativeMap.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team121/Records/RelativeMap.java
@@ -2,6 +2,8 @@ package ca.mcmaster.se2aa4.island.team121.Records;
 
 import ca.mcmaster.se2aa4.island.team121.Heading;
 import ca.mcmaster.se2aa4.island.team121.TileType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +13,7 @@ public class RelativeMap implements MapUpdater {
     Map<Point, TileType> relative_map;
     Point current_pos;
     Heading current_heading;
+    private final Logger logger = LogManager.getLogger();
 
     public RelativeMap() {
         this.relative_map = new HashMap<>();
@@ -22,18 +25,36 @@ public class RelativeMap implements MapUpdater {
     }
 
     public void updateFly() {
+        logger.info("** Updating position after flying");
+
         switch (current_heading) {
-            case NORTH -> current_pos = new Point(current_pos.x(), current_pos.y() - 1);
+            case NORTH -> current_pos = new Point(current_pos.x(), current_pos.y() + 1);
             case EAST -> current_pos = new Point(current_pos.x() + 1, current_pos.y());
-            case SOUTH -> current_pos = new Point(current_pos.x(), current_pos.y() + 1);
+            case SOUTH -> current_pos = new Point(current_pos.x(), current_pos.y() - 1);
             case WEST -> current_pos = new Point(current_pos.x() - 1, current_pos.y());
         }
+
+        logger.info("Current position: {}", current_pos);
         if (!relative_map.containsKey(current_pos))
             relative_map.put(current_pos, TileType.UNKNOWN);
     }
 
-    public void updateTurn() {
-        return;
+    public void updateTurn(Heading new_heading) {
+        logger.info("** Updating map after turning");
+
+        if (new_heading == current_heading) {
+            logger.error("The drone is facing the same direction");
+            return;
+        }
+
+        logger.info("Calling updateFly()");
+        updateFly();
+        current_heading = new_heading;
+        updateFly();
+        logger.info("Current heading: {}", current_heading);
+
+        if (!relative_map.containsKey(current_pos))
+            relative_map.put(current_pos, TileType.UNKNOWN);
     }
 
     public void updateScan() {
