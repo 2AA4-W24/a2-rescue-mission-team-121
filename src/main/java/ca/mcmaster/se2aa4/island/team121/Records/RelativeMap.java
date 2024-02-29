2210f2fe0a1c99508b9e80518ac2ec08358d1763
williampol <103951515+williampol@users.noreply.github.com>
2024-02-29 16:56:56 -0500

    feat: Change test case for RelativeMap

--- src/main/java/ca/mcmaster/se2aa4/island/team121/Records/RelativeMap.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team121/Records/RelativeMap.java
@@ -2,18 +2,15 @@ package ca.mcmaster.se2aa4.island.team121.Records;
 
 import ca.mcmaster.se2aa4.island.team121.Heading;
 import ca.mcmaster.se2aa4.island.team121.TileType;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class RelativeMap implements MapUpdater {
 
-    Map<Point, TileType> relative_map;
+    protected Map<Point, TileType> relative_map;
     Point current_pos;
     Heading current_heading;
-    private final Logger logger = LogManager.getLogger();
 
     public RelativeMap(Heading start_heading) {
         this.relative_map = new HashMap<>();
@@ -29,8 +26,6 @@ public class RelativeMap implements MapUpdater {
 
     @Override
     public void updateFly() {
-        logger.info("*** Updating position after flying");
-
         switch (current_heading) {
             case NORTH -> current_pos = new Point(current_pos.x(), current_pos.y() + 1);
             case EAST -> current_pos = new Point(current_pos.x() + 1, current_pos.y());
@@ -38,7 +33,6 @@ public class RelativeMap implements MapUpdater {
             case WEST -> current_pos = new Point(current_pos.x() - 1, current_pos.y());
         }
 
-        logger.info("Current position: {}", current_pos);
         if (!relative_map.containsKey(current_pos))
             relative_map.put(current_pos, TileType.UNKNOWN);
     }
@@ -48,18 +42,13 @@ public class RelativeMap implements MapUpdater {
     // but that case would most likely be handled before this method is called.
     @Override
     public void updateTurn(Heading new_heading) {
-        logger.info("** Updating map after turning");
-
         if (new_heading == current_heading) {
-            logger.error("The drone is facing the same direction");
             return;
         }
 
-        logger.info("Calling updateFly()");
         updateFly();
         current_heading = new_heading;
         updateFly();
-        logger.info("Current heading: {}", current_heading);
 
         if (!relative_map.containsKey(current_pos))
             relative_map.put(current_pos, TileType.UNKNOWN);
@@ -67,9 +56,7 @@ public class RelativeMap implements MapUpdater {
 
     @Override
     public void updateScan(TileType new_type) {
-        logger.info("** Updating map after scanning");
         relative_map.put(current_pos, new_type);
-        logger.info("New tile type: {}", relative_map.get(current_pos));
     }
 
     @Override
