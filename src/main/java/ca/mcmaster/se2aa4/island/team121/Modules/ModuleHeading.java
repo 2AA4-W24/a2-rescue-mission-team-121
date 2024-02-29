2a01225fc9ed72503ef3b410b96ec15051f0166a
williampol <103951515+williampol@users.noreply.github.com>
2024-02-29 17:19:14 -0500

    feat: Add new interface ModuleHeading

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team121/Modules/ModuleHeading.java
@@ -0,0 +1,9 @@
+package ca.mcmaster.se2aa4.island.team121.Modules;
+
+import org.json.JSONObject;
+import ca.mcmaster.se2aa4.island.team121.Heading;
+
+public interface ModuleHeading {
+
+    JSONObject getJSON(Heading heading);
+}
