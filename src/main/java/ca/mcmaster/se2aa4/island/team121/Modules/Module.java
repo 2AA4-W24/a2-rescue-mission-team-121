e0d20f3be4c866c7e4163e4efb111097b4a3831f
williampol <103951515+williampol@users.noreply.github.com>
2024-03-03 17:31:54 -0500

    feat: Add new abstract classes for modules

--- src/main/java/ca/mcmaster/se2aa4/island/team121/Modules/Module.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team121/Modules/Module.java
@@ -1,8 +1,14 @@
 package ca.mcmaster.se2aa4.island.team121.Modules;
 
+import ca.mcmaster.se2aa4.island.team121.Action;
 import org.json.JSONObject;
 
-public interface Module {
+public abstract class Module {
 
-    JSONObject getJSON();
+    protected JSONHandler jsoner;
+    protected Action action;
+
+    public JSONObject getJSON() {
+        return this.jsoner.format(action);
+    }
 }
---
+++ src/main/java/ca/mcmaster/se2aa4/island/team121/Modules/ModuleWithHeading.java
@@ -0,0 +1,21 @@
+package ca.mcmaster.se2aa4.island.team121.Modules;
+
+import ca.mcmaster.se2aa4.island.team121.Heading;
+import org.json.JSONObject;
+
+public abstract class ModuleWithHeading extends Module {
+
+    protected HeadingJSONHandler heading_jsoner;
+    protected Heading heading;
+
+    public ModuleWithHeading(Heading heading) {
+        this.heading = heading;
+    }
+
+    @Override
+    public JSONObject getJSON() {
+        JSONObject json = this.jsoner.format(action);
+        json = this.heading_jsoner.addHeading(json, this.heading);
+        return json;
+    }
+}
