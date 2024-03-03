262701bb3324cbaf12ac8fa9646d52733c0cbe70
williampol <103951515+williampol@users.noreply.github.com>
2024-03-03 17:30:53 -0500

    feat: Refactor Decision as Action

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team121/Action.java
@@ -0,0 +1,19 @@
+package ca.mcmaster.se2aa4.island.team121;
+
+public enum Action {
+    FLY("fly"),
+    SCAN("scan"),
+    STOP("stop"),
+    HEADING("heading"),
+    ECHO("echo");
+
+    private final String name;
+
+    private Action(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
