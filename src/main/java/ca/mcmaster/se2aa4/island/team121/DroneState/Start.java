cb6819dfe57d1a109b3fbbc5db57e29a7ab09a85
stefanpatroi <stefanpatroi@gmail.com>
2024-03-09 17:04:16 -0500

    Revert "State classes"
    This reverts commit c74b91801d3df836120c175af18e13fff0ac55c7.

--- src/main/java/ca/mcmaster/se2aa4/island/team121/DroneState/Start.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team121/DroneState/Start.java
@@ -1,31 +1,11 @@
 package ca.mcmaster.se2aa4.island.team121.DroneState;
 
-import ca.mcmaster.se2aa4.island.team121.Action;
 import ca.mcmaster.se2aa4.island.team121.Heading;
 import ca.mcmaster.se2aa4.island.team121.Modules.Flyer;
 import ca.mcmaster.se2aa4.island.team121.Modules.Radar;
-import ca.mcmaster.se2aa4.island.team121.Modules.Scanner;
-import ca.mcmaster.se2aa4.island.team121.Records.AttributeRecord;
-import ca.mcmaster.se2aa4.island.team121.Records.MovesRecord;
-import ca.mcmaster.se2aa4.island.team121.Records.RelativeMap;
-import org.json.JSONObject;
 
 public class Start {
     Radar radar = new Radar(Heading.SOUTH);
     Flyer fly = new Flyer();
-    private MovesRecord moves = new MovesRecord();
-    private RelativeMap map = new RelativeMap(Heading.EAST);
-    private Action next_action;
-    JSONObject decison = new JSONObject();
-
-    public JSONObject makeDecision()
-    {
-        if(moves.movesIsEmpty())
-        {
-            
-        }
-    }
-
-
 
 }
