b7323ef065ac1376fe8c8b778310c91ec52ad6f0
stefanpatroi <stefanpatroi@gmail.com>
2024-03-09 17:09:32 -0500

    Just the uturn class

--- src/main/java/ca/mcmaster/se2aa4/island/team121/DroneState/NorthScanning.java
+++
@@ -1,9 +0,0 @@
-package ca.mcmaster.se2aa4.island.team121.DroneState;
-
-import ca.mcmaster.se2aa4.island.team121.Modules.Flyer;
-import ca.mcmaster.se2aa4.island.team121.Modules.Scanner;
-
-public class NorthScanning {
-    Flyer fly = new Flyer();
-    Scanner scan = new Scanner();
-}
