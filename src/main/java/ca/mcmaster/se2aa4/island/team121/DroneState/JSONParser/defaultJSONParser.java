a7677962f2536da5747cccb1e78b7096775f0e0a
zayedu <umerzayed1@gmail.com>
2024-03-12 16:14:56 -0400

    feat: Add JSON Parser module

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team121/DroneState/JSONParser/defaultJSONParser.java
@@ -0,0 +1,59 @@
+package ca.mcmaster.se2aa4.island.team121.DroneState.JSONParser;
+
+import ca.mcmaster.se2aa4.island.team121.TileType;
+import org.json.JSONObject;
+
+public class defaultJSONParser implements JSONParser{
+
+    @Override
+    public TileType getScan(JSONObject response) {
+        if (response.has("extras")) {
+            JSONObject extras = response.getJSONObject("extras");
+            if(extras.has("biomes")){
+                return TileType.TileTypeOf(extras.getJSONArray("biomes").getString(0));
+            }
+        }
+        return null;
+
+    }
+
+    @Override
+    public boolean echoGround(JSONObject response) {
+       if(response.has("extras")){
+           JSONObject extras = response.getJSONObject("extras");
+           if(extras.has("found")){
+                String found = extras.getString("found");
+                if("OUT_OF_RANGE".equals(found)){
+                     return false;
+                }
+                else {
+                    return true;
+                }
+           }
+       }
+        return false;
+    }
+
+    @Override
+    public int echoDistance(JSONObject response) {
+        if(response.has("extras")){
+            JSONObject extras = response.getJSONObject("extras");
+            if(extras.has("range")){
+                return extras.getInt("range");
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int getCost(JSONObject response) {
+        if (response.has("data")) {
+            JSONObject data = response.getJSONObject("data");
+            if(data.has("cost")){
+                return data.getInt("cost");
+            }
+        }
+        return -1;
+    }
+
+}
