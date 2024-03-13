3027063ab09157f177e46081c2de2ea52bc2a920
williampol <103951515+williampol@users.noreply.github.com>
2024-03-13 14:35:05 -0400

    feat: JSONHandler to JSONFormatter

--- src/main/java/ca/mcmaster/se2aa4/island/team121/Modules/JSONFormatters/DefaultHeadingJSONFormatter.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team121/Modules/JSONFormatters/DefaultHeadingJSONFormatter.java
@@ -1,9 +1,9 @@
-package ca.mcmaster.se2aa4.island.team121.Modules.JSONHandlers;
+package ca.mcmaster.se2aa4.island.team121.Modules.JSONFormatters;
 
 import ca.mcmaster.se2aa4.island.team121.Heading;
 import org.json.JSONObject;
 
-public class DefaultHeadingJSONHandler implements HeadingJSONHandler {
+public class DefaultHeadingJSONFormatter implements HeadingJSONFormatter {
     @Override
     public JSONObject addHeading(JSONObject curr_json, Heading heading) {
         return curr_json.put("parameters", new JSONObject().put("direction", heading.getVector()));
