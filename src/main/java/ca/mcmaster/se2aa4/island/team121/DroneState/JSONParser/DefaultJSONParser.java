fb982f73578281fdb8138d46646ed6e6e5415212
zayedu <umerzayed1@gmail.com>
2024-03-20 23:15:01 -0400

    fix: add dependency for sonarqube

--- src/main/java/ca/mcmaster/se2aa4/island/team121/DroneState/JSONParser/DefaultJSONParser.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team121/DroneState/JSONParser/DefaultJSONParser.java
@@ -6,21 +6,25 @@ import java.util.ArrayList;
 import org.json.JSONArray;
 
 public class DefaultJSONParser implements JSONParser{
+    private final String extrasStr = "extras";
+    private final String creeksStr = "creeks";
+    private final String sitesStr = "sites";
+
     @Override
     public TileType getScan(JSONObject response) {
-         if(response.has("extras")){
-                JSONObject extras = response.getJSONObject("extras");
-                if(extras.has("creeks")){
-                    JSONArray creeks = extras.getJSONArray("creeks");
+         if(response.has(extrasStr)){
+                JSONObject extras = response.getJSONObject(extrasStr);
+                if(extras.has(creeksStr)){
+                    JSONArray creeks = extras.getJSONArray(creeksStr);
                     if(!creeks.isEmpty()){
                         return TileType.CREEK;
                     }
                 }
             }
-         if (response.has("extras")){
-             JSONObject extras = response.getJSONObject("extras");
-             if (extras.has("sites")){
-                if (!extras.getJSONArray("sites").isEmpty()){
+         if (response.has(extrasStr)){
+             JSONObject extras = response.getJSONObject(extrasStr);
+             if (extras.has(sitesStr)){
+                if (!extras.getJSONArray(sitesStr).isEmpty()){
                     return TileType.SITE;
                 }
              }
@@ -31,8 +35,8 @@ public class DefaultJSONParser implements JSONParser{
     @Override
     public String echoGround(JSONObject response) {
         String found = "NO_SCAN";
-        if (response.has("extras")) {
-            JSONObject extras = response.getJSONObject("extras");
+        if (response.has(extrasStr)) {
+            JSONObject extras = response.getJSONObject(extrasStr);
             if (extras.has("found")) {
                 found = extras.getString("found");
             }
@@ -42,8 +46,8 @@ public class DefaultJSONParser implements JSONParser{
 
     @Override
     public int echoDistance(JSONObject response) {
-        if(response.has("extras")){
-            JSONObject extras = response.getJSONObject("extras");
+        if(response.has(extrasStr)){
+            JSONObject extras = response.getJSONObject(extrasStr);
             if(extras.has("range")){
                 return extras.getInt("range");
             }
@@ -62,18 +66,18 @@ public class DefaultJSONParser implements JSONParser{
     @Override
     public ArrayList<String> getId(JSONObject response) {
         ArrayList<String> Ids = new ArrayList<>();
-        if (response.has("extras")) {
-            JSONObject extras = response.getJSONObject("extras");
-            if(extras.has("creeks")){
-                JSONArray creeks = extras.getJSONArray("creeks");
+        if (response.has(extrasStr)) {
+            JSONObject extras = response.getJSONObject(extrasStr);
+            if(extras.has(creeksStr)){
+                JSONArray creeks = extras.getJSONArray(creeksStr);
                 if (!creeks.isEmpty()){
                     for (int i = 0; i < creeks.length(); i++){
                         Ids.add(creeks.getString(i));
                     }
                 }
             }
-            if(extras.has("sites")){
-                JSONArray sites = extras.getJSONArray("sites");
+            if(extras.has(sitesStr)){
+                JSONArray sites = extras.getJSONArray(sitesStr);
                 if (!sites.isEmpty()){
                     for (int i = 0; i < sites.length(); i++){
                         Ids.add(sites.getString(i));
