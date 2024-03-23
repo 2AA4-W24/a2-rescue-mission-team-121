cc8b6af86afbe83054af99cb50e245be6f8ab993
williampol <103951515+williampol@users.noreply.github.com>
2024-03-23 18:00:32 -0400

    feat: Add StateTest

--- src/test/java/ca/mcmaster/se2aa4/island/team121/StateTest/StateTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team121/StateTest/StateTest.java
@@ -1,9 +1,17 @@
 package ca.mcmaster.se2aa4.island.team121.StateTest;
+
 import ca.mcmaster.se2aa4.island.team121.DroneState.State;
+import ca.mcmaster.se2aa4.island.team121.DroneState.Stop;
+import ca.mcmaster.se2aa4.island.team121.Modules.Flyer;
+import ca.mcmaster.se2aa4.island.team121.Modules.Radar;
+import ca.mcmaster.se2aa4.island.team121.Modules.Scanner;
+import ca.mcmaster.se2aa4.island.team121.Modules.Turner;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import ca.mcmaster.se2aa4.island.team121.Records.MapUpdater;
 import ca.mcmaster.se2aa4.island.team121.Records.RelativeMap;
 import ca.mcmaster.se2aa4.island.team121.BusinessDrivenObjects.Heading;
@@ -12,50 +20,43 @@ import ca.mcmaster.se2aa4.island.team121.BusinessDrivenObjects.Heading;
 class TestingState extends State {
     public TestingState(MapUpdater map) {
         super(map);
+        this.cycle.add(new Flyer(map));
+        this.cycle.add(new Turner(map, Heading.NORTH));
     }
 
     @Override
     public State getNext() {
-        return null;
+        return new Stop(map);
     }
 
     @Override
     public void update(JSONObject json) {
-
+        go_next = true;
     }
 }
 
 public class StateTest {
+
+    State state;
+
     @BeforeEach
     public void setup() {
-        MapUpdater map = new RelativeMap(Heading.NORTH);
-        State state = new TestingState(map);
-    }
-    @Test
-    public void testIsGoNext() {
-        MapUpdater map = new RelativeMap(Heading.NORTH);
-        State state = new TestingState(map);
-        assertEquals(false, state.isGoNext());
-    }
-    @Test
-    public void testExecute() {
-        MapUpdater map = new RelativeMap(Heading.NORTH);
-        State state = new TestingState(map);
-//        JSONObject json = state.execute();
-//        assertEquals(0, json.length());
-    }
-    @Test
-    public void testGetNext() {
-        MapUpdater map = new RelativeMap(Heading.NORTH);
-        State state = new TestingState(map);
-        assertEquals(null, state.getNext());
-    }
-    @Test
-    public void testUpdate() {
-        MapUpdater map = new RelativeMap(Heading.NORTH);
-        State state = new TestingState(map);
-        JSONObject json = new JSONObject();
-        state.update(json);
+        MapUpdater map = new RelativeMap(Heading.EAST);
+        state = new TestingState(map);
     }
 
+    @Test
+    public void testExecuteCycle() {
+        JSONObject expected_json = new JSONObject().put("action", "fly");
+        assertTrue(expected_json.similar(state.execute()));
+        expected_json = new JSONObject().put("action", "heading");
+        expected_json.put("parameters", new JSONObject().put("direction", "N"));
+        assertTrue(expected_json.similar(state.execute()));
+    }
+
+    @Test
+    public void testUpdate() {
+        state.update(new JSONObject());
+        assertEquals(true, state.isGoNext());
+    }
 }
